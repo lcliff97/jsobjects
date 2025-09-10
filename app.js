dd60da15233f05ca1734ae74a909593543b81ddb
lcliff97 <louiecliff97@outlook.com>
2025-09-10 14:49:29 +0100

    objects code

--- app.js
+++ app.js
@@ -8,3 +8,30 @@ const myHotel = {
 };
 
 console.log(myHotel.colour);
+
+const person = {
+  name: "Micheal Jackson",
+  dance: function (name) {
+    console.log(`moonwalks whilst going "heehee"` + "hello" + name);
+  },
+};
+
+person.dance("Connor");
+
+const animal = {
+  breed: "platypus",
+  age: 4,
+  location: "Australia",
+};
+
+console.log(animal.age);
+
+const building = {
+  height: "very big",
+  rooms: 430584068,
+  transform: function () {
+    console.log(`transforms into some type of Decepticon`);
+  },
+};
+
+building.transform();
